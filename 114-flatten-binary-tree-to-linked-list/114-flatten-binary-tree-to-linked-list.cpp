5ce4db2ad511b8dd85458b0cbd103f4f764b1798
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-08-15 17:09:36 +0530

    Time: 12 ms (40.65%), Space: 12.7 MB (38.63%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -11,22 +11,49 @@
  */
 class Solution {
 public:
-    TreeNode* head=NULL;
-    void flat(TreeNode* root){
-        if(!root){return;}
-        if(!head){
-            head = root;
+    TreeNode* flat(TreeNode* root){
+//         if(!root->left && !root->right)return root;
+        
+        
+//         TreeNode* last1 = NULL;
+//         TreeNode* last2 = NULL;
+        
+//         if(root->left)last1 = flat(root->left);
+//         if(root->right)last2 = flat(root->right);
+        
+//         if(last1){
+//             last1 ->right  = root->right;
+//             root->right = root->left;
+//         }else{
+//             return last2;
+//         }
+//         root->left = NULL;
+        
+//         return !last2?last1:last2;
+        
+        if(!root)return root;
+        
+        TreeNode* last1 = flat(root->left);
+        TreeNode* last2 = flat(root->right);
+        
+        if(!last1 && !last2){
+            return root;
+        }else if(last2 && last1){
+            
+           last1 ->right  = root->right;
+           root->right = root->left;
+           root->left = NULL;
+           return last2;
+            
+        }else if(!last1){
+            return last2;
         }else{
-            head->right = root;
-            head = head->right;
+            root->right = root->left;
+            root->left = NULL;
+            return last1;
         }
-        TreeNode* temp = root->right;
-        flat(root->left);
-        root->left = NULL;
-        flat(temp);
     }
     void flatten(TreeNode* root) {
         flat(root);
-        return;
     }
 };
