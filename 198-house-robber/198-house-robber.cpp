2c3c30f9538f6441acbfec2ba7ad5ea715413d2f
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-08-20 16:03:02 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (95.27%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
-    // int robbed(vector<int>& nums, int i){
-    //     if(i>=nums.size()){return 0;} 
-    //     return max(nums[i]+robbed(nums,i+2), robbed(nums,i+1));
-    // }
     int rob(vector<int>& nums) {
-        //return robbed(nums, 0 );
-        if(nums.size()==1){return nums[0];}
-        
-        int pr1 = max(nums[0],nums[1]);
-        int pr2 = nums[0];
-        
-        for(int i=2;i<nums.size();i++){
-            int t = pr1;
-            pr1 = max(pr1,pr2+nums[i]);
-            pr2 = t;
-        }  
-        return pr1;
+        for(int i = 1; i< nums.size(); i++){
+            nums[ i ] = max(nums[i-1],nums[i]+ (i>1?nums[i-2]:0) );
+        }
+        return nums[nums.size()-1];
     }
 };
