cb3e3f3654b214f911eb4293b929c577f22ab1b8
Ritik Tripathi <88445142+CodeRitik189@users.noreply.github.com>
2022-09-08 19:20:09 +0530

    Time: 180 ms (37.37%), Space: 6 MB (80.37%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int ans = 0;      
-    void find(int n, int ct, vector<int>& vis){
-        if(ct == n){
-            ans++;
-            return;
-        }
+    int ans = 0;     
+    int find(int n, int ct, vector<int>& vis){
+        if(ct == n)return 1;
         
+        int ans = 0;
         for(int i = 1; i<=n; i++){
             if(!vis[i]){
                 vis[i] = 1;
                 if( i % (ct+1) == 0 || (ct+1)%i == 0)
-                  find(n, ct+1, vis);
+                  ans += find(n, ct+1, vis);
                 vis[i] = 0;
             }
         }
+        return ans;
     }
     
     int countArrangement(int n) {
         vector<int>vis(n+1,0);
-        
-        find(n, 0, vis);
-        return ans;
+        return find(n, 0, vis);
     }
 };
